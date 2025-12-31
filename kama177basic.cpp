206f013eb12ce2477f98d2fa3e9ffb5ae17d87f4
xyc0833 <2031362123@qq.com>
2025-12-31 16:12:38 +0800

    重新做了修改

--- kama177basic.cpp
+++ kama177basic.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    unordered_map<int,vector<int> > map;
+    int n,m,s,t;
+    cin>>n>>m;
+    for(int i = 0;i<m;i++){
+        cin>>s>>t;
+        map[s].push_back(t);
+    }
+    for(auto abc : map){
+        int x = abc.first;
+        cout<<x<<"-> ";
+
+        vector<int> k = abc.second;
+        for(auto x:k){
+            cout<<x<<' ';
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
